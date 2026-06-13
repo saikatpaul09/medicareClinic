d4b6d39cdadc05908715e98e8f96938ec3797d08
Saikat Paul <saikat@nivoda.com>
2026-06-13 22:43:15 +0530

    Added migration for all tables

--- medicare-website/src/components/my-account/EditProfile.tsx
+++ medicare-website/src/components/my-account/EditProfile.tsx
@@ -6,7 +6,6 @@ import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { Button } from "../button/Button";
 import theme from "../../theme";
@@ -106,7 +105,6 @@ const EditProfilDialogBox = ({
   handleClose,
   open,
   data,
-  key,
   mutuateUpdateProfile,
   isPending,
 }: {
@@ -120,7 +118,7 @@ const EditProfilDialogBox = ({
     age: string;
     gender: Gender;
   };
-  key: string;
+
   isPending: boolean;
   mutuateUpdateProfile: (formData: ProfileForm) => void;
 }) => {
@@ -134,7 +132,6 @@ const EditProfilDialogBox = ({
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-slide-description"
       role="alertdialog"
-      key={key}
       sx={{
         padding: theme.spacing(5),
       }}
@@ -144,139 +141,135 @@ const EditProfilDialogBox = ({
       </DialogTitle>
       <Divider />
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              flexWrap: "wrap",
-              gap: "10px",
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            gap: "10px",
+          }}
+        >
+          <TextField
+            label="First Name"
+            required
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            sx={{ width: "270px" }}
+            value={formData.firstName}
+            onChange={(e) =>
+              setFormData({ ...formData, firstName: e.target.value })
+            }
+          />
+          <TextField
+            label="Last Name"
+            required
+            sx={{ width: "250px" }}
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={formData.lastName}
+            onChange={(e) =>
+              setFormData({ ...formData, lastName: e.target.value })
+            }
+          />
+          <TextField
+            label={error ? "Please enter proper email Id" : "Email"}
+            required
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            sx={{ width: "270px" }}
+            error={!!error}
+            value={formData.email}
+            onBlur={() => {
+              // Trim whitespace to handle accidental trailing spaces
+              const trimmedEmail = formData.email.trim();
+              if (!trimmedEmail) {
+                setError("Email address is required.");
+              } else if (!EMAIL_REGEX.test(trimmedEmail)) {
+                setError("Please enter a valid email address.");
+              } else {
+                setError(""); // Clear error if validation passes
+              }
             }}
-          >
-            <TextField
-              label="First Name"
-              required
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              sx={{ width: "270px" }}
-              value={formData.firstName}
-              onChange={(e) =>
-                setFormData({ ...formData, firstName: e.target.value })
-              }
-            />
-            <TextField
-              label="Last Name"
-              required
-              sx={{ width: "250px" }}
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              value={formData.lastName}
-              onChange={(e) =>
-                setFormData({ ...formData, lastName: e.target.value })
-              }
-            />
-            <TextField
-              label={error ? "Please enter proper email Id" : "Email"}
-              required
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              sx={{ width: "270px" }}
-              error={!!error}
-              value={formData.email}
-              onBlur={() => {
-                // Trim whitespace to handle accidental trailing spaces
-                const trimmedEmail = formData.email.trim();
-                if (!trimmedEmail) {
-                  setError("Email address is required.");
-                } else if (!EMAIL_REGEX.test(trimmedEmail)) {
-                  setError("Please enter a valid email address.");
-                } else {
-                  setError(""); // Clear error if validation passes
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          <TextField
+            label="Age"
+            sx={{ width: "270px" }}
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={formData.age}
+            onChange={(e) => setFormData({ ...formData, age: e.target.value })}
+          />
+          <TextField
+            label="Phone No."
+            sx={{ width: "250px" }}
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={formData.phone}
+            onChange={(e) =>
+              setFormData({ ...formData, phone: e.target.value })
+            }
+          />
+          <Box>
+            <Typography>Gender</Typography>
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    disabled
+                    checked={formData.gender === "MALE"}
+                    onChange={() =>
+                      setFormData({
+                        ...formData,
+                        gender: "MALE",
+                      })
+                    }
+                  />
                 }
-              }}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-            />
-            <TextField
-              label="Age"
-              sx={{ width: "270px" }}
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              value={formData.age}
-              onChange={(e) =>
-                setFormData({ ...formData, age: e.target.value })
-              }
-            />
-            <TextField
-              label="Phone No."
-              sx={{ width: "250px" }}
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              value={formData.phone}
-              onChange={(e) =>
-                setFormData({ ...formData, phone: e.target.value })
-              }
-            />
-            <Box>
-              <Typography>Gender</Typography>
-              <FormGroup row>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      disabled
-                      checked={formData.gender === "MALE"}
-                      onChange={() =>
-                        setFormData({
-                          ...formData,
-                          gender: "MALE",
-                        })
-                      }
-                    />
-                  }
-                  label="Male"
-                />
-                <FormControlLabel
-                  required
-                  control={
-                    <Checkbox
-                      checked={formData.gender === "FEMALE"}
-                      disabled
-                      onChange={() =>
-                        setFormData({
-                          ...formData,
-                          gender: "FEMALE",
-                        })
-                      }
-                    />
-                  }
-                  label="Female"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={formData.gender === "OTHERS"}
-                      disabled
-                      onChange={() =>
-                        setFormData({
-                          ...formData,
-                          gender: "OTHERS",
-                        })
-                      }
-                    />
-                  }
-                  label="Others"
-                />
-              </FormGroup>
-            </Box>
+                label="Male"
+              />
+              <FormControlLabel
+                required
+                control={
+                  <Checkbox
+                    checked={formData.gender === "FEMALE"}
+                    disabled
+                    onChange={() =>
+                      setFormData({
+                        ...formData,
+                        gender: "FEMALE",
+                      })
+                    }
+                  />
+                }
+                label="Female"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formData.gender === "OTHERS"}
+                    disabled
+                    onChange={() =>
+                      setFormData({
+                        ...formData,
+                        gender: "OTHERS",
+                      })
+                    }
+                  />
+                }
+                label="Others"
+              />
+            </FormGroup>
           </Box>
-        </DialogContentText>
+        </Box>
       </DialogContent>
       <DialogActions sx={{ m: 0, p: 2 }}>
         <Button
