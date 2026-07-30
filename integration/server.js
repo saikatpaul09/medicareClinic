beca02523fa77e459f64400259d05d27cbb341e5
Saikat Paul <saikat@nivoda.com>
2026-07-30 14:10:54 +0530

    minor changes

--- integration/server.js
+++ integration/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 //cors midleware
 app.use(
   cors({
-    origin: ["http://localhost", "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: ["POST", "GET", "PUT", "DELETE"],
   }),
